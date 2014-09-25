86778f9f2924fb5242c1a1d02e5d95f909365cba
BenFradet <benjamin.fradet@gmail.com>
2014-09-25 20:30:13 +0200

    finalized post

--- main.R
+++ main.R
@@ -15,6 +15,7 @@ wdbc$diagnosis <- factor(wdbc$diagnosis, levels = c('B', 'M'),
 wdbc <- wdbc[sample(nrow(wdbc)), ]
 
 # print out the range for each mean feature
+cat("\nRanges of the 10 first features in the dataset:\n")
 print(lapply(wdbc[2:11], function(x) { max(x) - min(x) }))
 
 # normalize our numeric features removing the diagnosis feature since it's a
@@ -22,6 +23,7 @@ print(lapply(wdbc[2:11], function(x) { max(x) - min(x) }))
 wdbcNormalized <- as.data.frame(scale(wdbc[-1]))
 
 # print out the mean of a few features
+cat("\n\nMeans for 3 features after scaling:\n")
 print(summary(wdbcNormalized[c('radius_mean', 'area_worst', 'symmetry_se')]))
 
 # take the first 427 examples as our training set without the diagnosis feature
@@ -36,14 +38,22 @@ wdbcTestLabels <- wdbc[428:569, 1]
 
 # use the knn function of the class package
 k <- 21
-wdbcPredictedClass <- knn(train = wdbcTraining,
-                          test = wdbcTest,
-                          cl = wdbcTrainingLabels,
-                          k)
+wdbcPredictedLabels <- knn(train = wdbcTraining,
+                           test = wdbcTest,
+                           cl = wdbcTrainingLabels,
+                           k)
 
 # compute the percentage of right predictions
-actualVsPredicted <- cbind(wdbcTestLabels, wdbcPredictedClass)
+actualVsPredicted <- cbind(wdbcTestLabels, wdbcPredictedLabels)
 colnames(actualVsPredicted) <- c('actual', 'predicted')
 percentage <- sum(apply(actualVsPredicted, 1,
                         function(row) { ifelse(row[1] == row[2], 1, 0) }
                         )) / dim(actualVsPredicted)[1]
+cat("\n\nPercentage of examples correctly classified:\n")
+print(percentage)
+
+# display a crossTable
+cat("\n\nCrosstable of the actual classes of the examples in the test set",
+    "versus the predicted classes:\n")
+CrossTable(x = wdbcTestLabels, y = wdbcPredictedLabels,
+           prop.chisq = F, dnn = c('actual', 'predicted'))
